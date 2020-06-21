56c736645179e29211e1d4c9e505ab75524d27a0
azambori <64330523+azambori@users.noreply.github.com>
2020-06-21 15:08:42 +0200

    elso mukodo verzio, mutatja nyillal merre áll a pocok

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P0, 10)
-    basic.showString("S")
+    basic.showArrow(ArrowNames.East)
 })
 input.onButtonPressed(Button.AB, function () {
     pins.servoWritePin(AnalogPin.P0, 160)
-    basic.showString("R")
+    basic.showString("Reset")
 })
 input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P0, 90)
-    basic.showString("D")
+    basic.showArrow(ArrowNames.West)
 })
 pins.servoWritePin(AnalogPin.P0, 160)
 basic.pause(500)
-pins.servoWritePin(AnalogPin.P0, 90)
+pins.servoWritePin(AnalogPin.P0, 10)
 basic.pause(500)
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
